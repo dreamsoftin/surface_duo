7655b16c956c8b956856e41f78051182a4517d56
Sachin <sachinganesh@outlook.com>
2020-08-15 12:08:41 +0530

    init

---
+++ example/android/app/src/main/kotlin/dev/dsi/duosdk/surface_duo_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.dsi.duosdk.surface_duo_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/surface_duo.dart
@@ -0,0 +1,14 @@
+
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class SurfaceDuo {
+  static const MethodChannel _channel =
+      const MethodChannel('surface_duo');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+}
